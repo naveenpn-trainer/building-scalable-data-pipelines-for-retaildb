d36194183f027439fa3344d2b3c8e69d990a6197
naveenpn.trainer@gmail.com <naveenpn.trainer@gmail.com>
2024-09-05 11:04:44 +0530

    Add code to upload files to S3

---
+++ projects/cloud-file-management/copy_to_s3.py
@@ -0,0 +1,38 @@
+import os
+import boto3
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+BUCKET_NAME = "sunlife_bucket"
+LOCAL_DIRECTORY = "../../dataset/processed/"
+
+s3_client = boto3.client('s3')
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory and event.src_path.endswith('.csv'):
+            file_path = event.src_path
+            file_name = os.path.basename(file_path)
+            try:
+                s3_client.upload_file(file_path, BUCKET_NAME, file_name)
+                print(f"Uploaded {file_name} to {BUCKET_NAME}")
+            except Exception as e:
+                print(f"Error uploading {file_name}: {e}")
+
+
+def main():
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=LOCAL_DIRECTORY, recursive=True)
+    observer.start()
+    print(f"Watching directory: {LOCAL_DIRECTORY}")
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == "__main__":
+    main()
